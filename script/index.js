88ed1bcf27c81b7ae61f305d33a85f7934887511
Vashti Overmiller <vovermiller@gmail.com>
2019-11-12 10:03:30 -0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
+const START_HP = 20;
+const START_SP = 10;
+const START_ATK = 5;
+const START_DEF = 5;
+const START_TEK = 5;
+
 class Fighter {
   constructor(name, charaName) {
     //'contructor' is in all JS classes
