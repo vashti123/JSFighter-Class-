0270696665c093a981b1216e5f12b6b2798ab97d
Gregory <grego2020632@live.ytech.edu>
2019-11-12 10:42:29 -0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,8 +1,31 @@
 
+const START_HP = 20;
+const START_SP = 10;
+const DEFAULT_ATK = 5;
+const DEFAULT_DEF = 5;
+const DEFAULT_TEK = 5;
+
+const P0NAME = "Crash"
+const P0CHARA = "crashr"
+const P1NAME = "Sam"
+const P1CHARA = "saml"
+
+let playerTurn = false;
+let logging = true;
+
+
+class Fighter {
+  constructor(name, charaName) {
+    //'contructor' is in all JS classes
+    // It gets run immediately when a new object is created from a class
+
+    // Set all of our default values for this new fighter here
+    this.name = name;
+    this.hp = START_HP;
+    this.sp = START_SP;
     this.atk = DEFAULT_ATK;
     this.def = DEFAULT_DEF;
     this.tek = DEFAULT_TEK;
-
     this.charaName = charaName;
   }
   attack(target) {
