31f6adbefc9e606fc3a3009133ec913634f05fbe
xstxphyx <55801404+xstxphyx@users.noreply.github.com>
2019-11-15 19:05:50 -0500

    Update index.js
    Added comments and fixed my outdated code.

--- script/index.js
+++ script/index.js
@@ -1,21 +1,23 @@
-
+//sets constants bars
 const START_HP = 20;
 const START_SP = 10;
 const DEFAULT_ATK = 5;
-const DEFAULT_DEF = 5
-const DEFAULT_TEK = 5
+const DEFAULT_DEF = 5;
+const DEFAULT_TEK = 5;
 
+//sets constants names
 const P0NAME = 'Crash'
 const P0CHARA = 'crashr'
 const P1NAME = 'Sam'
 const P1CHARA = 'saml'
 
+let playerTurn = false;
+let logging = true;
+
 let Player0;
 let Player1;
 
-let playerTurn = false;
-let logging = false;
-
+// sets variables for the boxes
 let gameBox;
 let headerBox;
 let graphicsBox;
@@ -25,7 +27,9 @@ let outputBox;
 
 class Fighter {
   constructor(name, charaName) {
-    //'contructor' is in all JS classes. It is run immediately when a new object is created from a class
+    //'contructor' is in all JS classes
+    // It gets run immediately when a new object is created from a class
+
     // Set all of our default values for this new fighter here
     this.name = name;
     this.hp = START_HP;
@@ -35,27 +39,32 @@ class Fighter {
     this.tek = DEFAULT_TEK;
     this.charaName = charaName;
   }
-  attack(target) {
-    console.log(this.name + ' attacked ' + target.name)
 
+  //this logs who attacked who
+  attack(target) {
+    console.log(this.name + ' attacked ' + target.name);
   }
+
   single(target) {
     this.attack(target);
   }
+
   double(target) {
     this.attack(target);
     this.attack(target);
   }
+
+  //this logs that they recovered
   recover() {
-    console.log('Recovered!')
+    console.log('Recovered!');
   }
 }
 
-
 function startup() {
-  Player0 = new Fighter('Crash', 'crashr');
-  Player1 = new Fighter('Sam', 'saml');
+  Player0 = new Fighter(P0NAME, P0CHARA);
+  Player1 = new Fighter(P1NAME, P1CHARA);
 
+  //this makes a shortcut and cleans the code
   gameBox = document.getElementById('gameBox');
   headerBox = document.getElementById('headerBox');
   graphicsBox = document.getElementById('graphicsBox');
@@ -63,24 +72,53 @@ function startup() {
   controlsBox = document.getElementById('controlsBox');
   outputBox = document.getElementById('outputBox');
 
+  //this shows the images in the graphics box
   graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
   graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
-
-  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
-  console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
-
+  //this calls the funtion 'showControls()'
   showControls();
+  
+  //this logs their name and attack
+  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk);
+  console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk);
 }
 
 function showControls() {
   //Player0 is FALSE, Player1 is TRUE.
   if (playerTurn) {
+    //makes the buttons for crashes turn
     controlsBox.innerHTML = '<button type="button" onclick= "Player0.single(Player1)">Single Attack</button>'
+    //logs crashes turn
     console.log("Crash's turn.");
   }
   else {
+    //makes the button for sams turn
     controlsBox.innerHTML = '<button type="button" onclick= "Player1.single(Player0)">Single Attack</button>'
+    //logs sams turn
     console.log("Sam's turn.");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+MHW = 'delicious'
+
+MHWoutput > MHWinput
+
+*/
